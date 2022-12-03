966d31dff75760ac8310c8951d45d00078028e35
Joginder <joginder.director@gmail.com>
2022-12-03 15:30:42 +0530

    testing project

--- pages/index.js
+++ pages/index.js
@@ -1,50 +1,51 @@
 import Head from 'next/head'
-import HomeBanner from "../components/banners/HomeBanner"
-import SiteBanner from "../components/siteBanner/SiteBanner"
-import TutorialTab from '../components/tutorialTab/TutorialTab'
-import Goals from "../components/goals/Goals"
-import Issue from '../components/issues/Issues'
-import Blogs from "../components/blogs/Blogs"
-import PopularTutorial from "../components/popularTutorial/PopularTutorial"
-import TrainingPlatform from "../components/training/TrainingPlatform"
+// import HomeBanner from "../components/banners/HomeBanner"
+// import SiteBanner from "../components/siteBanner/SiteBanner"
+// import TutorialTab from '../components/tutorialTab/TutorialTab'
+// import Goals from "../components/goals/Goals"
+// import Issue from '../components/issues/Issues'
+// import Blogs from "../components/blogs/Blogs"
+// import PopularTutorial from "../components/popularTutorial/PopularTutorial"
+// import TrainingPlatform from "../components/training/TrainingPlatform"
 import Layout from "../layout/Layout"
-import axios from 'axios';
-import { issue, blogs, skills } from '../services/services'
+// import axios from 'axios';
+// import { issue, blogs, skills } from '../services/services'
 
-export default function Home({metaTags, issueData, blogsData, skillData}) {
+export default function Home({}) {
   return (
     <div >
       <Head>
         <title>Home</title>
-        <meta name="keywords" content={metaTags[0].meta_tags} />
-        <meta name="description" content={metaTags[0].meta_description} />
+        {/* <meta name="keywords" content={metaTags[0].meta_tags} /> */}
+        {/* <meta name="description" content={metaTags[0].meta_description} /> */}
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout>
-        <HomeBanner />
+        <h1>Hello world</h1>
+        {/* <HomeBanner />
         <SiteBanner />
         <TutorialTab skillData={skillData} />
         <Goals />
         <Issue issueData={issueData} />
         <Blogs blogsData={blogsData} />
         <PopularTutorial />
-        <TrainingPlatform />
+        <TrainingPlatform /> */}
       </Layout>
     </div>
   )
 }
 
-export async function getServerSideProps(context) {
-  const res = await axios.get(`${process.env.NEXT_APP_API_URL}/getmetatags/2`, {
-      headers: {
-          "X-API-KEY": process.env.NEXT_APP_API_KEY
-      }
-    });
-  const metaTags = res.data
-  const issueData = (await issue()) || []
-  const blogsData = (await blogs()) || []
-  const skillData = (await skills()) || []
-  return {
-    props: {metaTags, issueData, blogsData, skillData}, // will be passed to the page component as props
-  }
-}
+// export async function getServerSideProps(context) {
+  // const res = await axios.get(`${process.env.NEXT_APP_API_URL}/getmetatags/2`, {
+  //     headers: {
+  //         "X-API-KEY": process.env.NEXT_APP_API_KEY
+  //     }
+  //   });
+  // const metaTags = res.data
+  // const issueData = (await issue()) || []
+  // const blogsData = (await blogs()) || []
+  // const skillData = (await skills()) || []
+  // return {
+  //   props: {metaTags, issueData, blogsData, skillData}, // will be passed to the page component as props
+  // }
+// }
